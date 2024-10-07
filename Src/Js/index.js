b77766faab57b4517dea0a1bbee7245f8a2c7996
Glauedson <glauedson18s@gmail.com>
2024-10-07 13:58:05 -0300

    BuscarPaises V1.1

---
+++ Src/Js/index.js
@@ -0,0 +1,89 @@
+window.addEventListener("load", function() {
+    const precarregamento = document.getElementById("preloader")
+    const content = document.getElementById("content")
+
+    setTimeout(function() {
+        precarregamento.style.display = "none"
+        content.style.display = "block"
+    }, 2000);
+})
+
+var todosPaises = []
+var todosPaisesDiv = document.querySelector(".todosPaises")
+var qtPaises = document.querySelector("#qtPaises")
+
+async function consultarPaises(url) {
+  var resposta = await fetch("https://restcountries.com/v3.1/" + url)
+  var dados = await resposta.json()
+
+  todosPaises = dados
+  qtPaises.innerHTML = todosPaises.length
+  mostrarPaises(todosPaises)
+
+}
+
+function mostrarPaises(paises) {
+
+  todosPaisesDiv.innerHTML = "";
+  for (pais of paises) {
+
+    var paisDiv = document.createElement("div")
+    paisDiv.classList.add("pais")
+
+    paisDiv.innerHTML = `
+      <img
+          width="150"
+          src="${pais.flags.png}"
+          alt="${pais.flags.alt}"
+      />
+      <p>${pais.translations.por.common}</p>
+    `
+
+    paisDiv.id = pais.cca2
+
+    paisDiv.addEventListener("click", abrirPaginaDetalhes)
+
+    todosPaisesDiv.appendChild(paisDiv)
+  }
+}
+
+function abrirPaginaDetalhes(event){
+  var paisId;
+
+  if (event.target.className != "pais") {
+    paisId = event.target.parentElement.id
+  } else {
+    paisId = event.target.id
+  }
+
+  window.location.href = `./maisDetalhes.html?id=${paisId}`
+}
+
+function buscarPaises(value) {
+  var paisBuscados = []
+
+  for (pais of todosPaises) {
+    var nome = pais.translations.por.common.toLowerCase()
+    if (nome.startsWith(value.toLowerCase())) {
+      paisBuscados.push(pais)
+    }
+  }
+  todosPaisesDiv.innerHTML = ""
+  qtPaises.innerHTML = paisBuscados.length
+  mostrarPaises(paisBuscados)
+
+}
+
+function pesquisarPaisPorFiltro(value) {
+
+  var url = ""
+  if(value != "all"){
+    url = "region/" + value
+  }else{
+    url = "all"
+  }
+  consultarPaises(url)
+
+}
+
+consultarPaises("all")
---
+++ Src/Js/maisDetalhes.js
@@ -0,0 +1,77 @@
+// API de paises
+const lerURL = window.location.search
+const URL = new URLSearchParams(lerURL)
+const paisId = URL.get('id').toLowerCase()
+
+var TopoNome = document.querySelector("#nomeTopo")
+var NomePais = document.querySelector("#nome-pais")
+var Capital = document.querySelector("#capial")
+var Bandeira = document.querySelector("#bandeira-pais")
+var Idioma = document.querySelector("#Idioma")
+var Moeda = document.querySelector("#Moeda")
+var Continente = document.querySelector("#Continente")
+var Populacao = document.querySelector("#Populacao")
+var Area = document.querySelector("#Area")
+
+//API Wikipedia
+var InfoPais = document.querySelector("#wiki-info-pais")
+var Background = document.getElementById("background")
+var FotoCapital = document.getElementById("Foto-Capital")
+var InfoCapital = document.getElementById("Info-Capital")
+
+
+async function codigoPrincipal() {
+    // paises -------------
+    var API = await fetch("https://restcountries.com/v3.1/alpha/" + paisId)
+    var dados = await API.json()
+    var subdados = dados[0]
+    var IdiomaDado = Object.values(subdados.languages).join(`, `)
+    var MoedaDado = Object.values(subdados.currencies)[0].name
+    const lat = subdados.latlng[0]; 
+    const lng = subdados.latlng[1]; 
+
+
+    TopoNome.innerHTML = `${subdados.translations.por.common} - ${paisId.toUpperCase()}`
+    NomePais.innerHTML = `${subdados.translations.por.common.toUpperCase()}`
+    Capital.innerHTML = `${subdados.capital}`
+    Bandeira.innerHTML = ` <img src="${subdados.flags.svg}" /> `
+    Idioma.innerHTML = `${IdiomaDado.toUpperCase()}`
+    Moeda.innerHTML = `${MoedaDado.toUpperCase()}`
+    Continente.innerHTML = `${subdados.continents[0].toUpperCase()}`
+    Populacao.innerHTML = `${subdados.population}`
+    Area.innerHTML = `${subdados.area} Km²`
+
+  
+    // wikipedia --------------
+    var urlwiki = await fetch(`https://pt.wikipedia.org/api/rest_v1/page/summary/${subdados.translations.por.common}`)
+    var dadoswiki = await urlwiki.json()
+    var CapitalWiki = await fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${subdados.capital}`)
+    var CapitalDados = await CapitalWiki.json()
+    var CapitalWikiBR = await fetch(`https://pt.wikipedia.org/api/rest_v1/page/summary/${subdados.capital}`)
+    var CapitalDadosBR = await CapitalWikiBR.json()
+
+    try{
+    Background.style.backgroundImage = `url(${CapitalDados.originalimage.source})`
+    } catch (error){
+    Background.style.backgroundImage = `url(../Imagens/img-paises/main-erro.jpg)`
+    }
+
+    InfoPais.innerHTML = `${dadoswiki.extract}`
+    FotoCapital.style.backgroundImage =  `url(${CapitalDados.originalimage.source})`
+    InfoCapital.innerHTML = `${CapitalDadosBR.extract}`
+
+    // Leaflet 
+    var map = L.map('map').setView([lat, lng], 6);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    L.marker([lat, lng]).addTo(map)
+        .bindPopup(`<b>${subdados.capital}</b><br>Capital de ${subdados.translations.por.common}`)
+        .openPopup();
+
+  }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    codigoPrincipal();
+});
